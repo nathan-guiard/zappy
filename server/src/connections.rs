5916e3908db25fd63565cbeaf2ac9501fc2ca1ec
Nathan Guiard <nguiard@student.42.fr>
2024-03-05 15:08:31 +0100

    server: Improved corrections, but the disconnections are not handled

--- server/src/connections.rs
+++ server/src/connections.rs
@@ -6,11 +6,11 @@
 /*   By: nguiard <nguiard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 14:12:44 by nguiard           #+#    #+#             */
-/*   Updated: 2024/03/05 12:21:25 by nguiard          ###   ########.fr       */
+/*   Updated: 2024/03/05 15:07:12 by nguiard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-use std::io::Error;
+use std::{fs::File, io::{BufRead, BufReader, Error, ErrorKind, Read}, os::fd::FromRawFd, str::from_utf8};
 
 use epoll::Events;
 use libc::*;
@@ -23,68 +23,98 @@ pub struct ServerConnection {
 	pub socket_fd: i32,
 }
 
-pub fn init_socket(port: u16) -> ServerConnection {
-	let mut result: ServerConnection;
-	result = unsafe { std::mem::zeroed() };
-	result.socket_fd = unsafe { socket(AF_INET, SOCK_STREAM, 0) };
-	if result.socket_fd < 0 {
-		// Return some kind of error
-		todo!();
-	}
-
-	result.soc_addr.sin_addr.s_addr = INADDR_ANY.to_be();
-	result.soc_addr.sin_port = port.to_be();
-	result.soc_addr.sin_family = AF_INET as u16;
-
-	if unsafe { fcntl(result.socket_fd, F_SETFL, O_NONBLOCK) }	< 0 {
-		unsafe { libc::close(result.socket_fd) };
-		// Return some kind of error;
-		todo!();
-	}
-
-	if unsafe { bind(result.socket_fd,
-		&result.soc_addr as *const _ as *const _,
-		std::mem::size_of_val(&result.soc_addr) as u32) } != 0 {
-		unsafe { libc::close(result.socket_fd) };
-		// Return some kind of error
-		todo!();
-	}
-
-	if unsafe { listen(result.socket_fd, 32) } != 0 {
-		unsafe { libc::close(result.socket_fd) };
-		// Return some kind of error
-		todo!();
-	}
-
-	return result;
-}
-
-pub fn get_new_connections(con_data: &ServerConnection, watcher: &mut Watcher)
-	-> Result<(), Error> {
-	// Check for new connections (maybe another function)
-	loop {
-		let new_connection = unsafe {
-			accept(con_data.socket_fd,
-				std::ptr::null_mut(),
-				std::ptr::null_mut())
-		};
-		if new_connection == -1 {
-			let last_error = Error::last_os_error();
-			if let Some(errno) = last_error.raw_os_error() {
-				if errno == EWOULDBLOCK || errno == EAGAIN {
-					println!("No (more) incoming connections");
-					return Ok(());
-				}
-			}
-			return Err(last_error);
+impl ServerConnection {
+	pub fn init_socket(port: u16) -> ServerConnection {
+		let mut result: ServerConnection;
+		result = unsafe { std::mem::zeroed() };
+		result.socket_fd = unsafe { socket(AF_INET, SOCK_STREAM, 0) };
+		if result.socket_fd < 0 {
+			// Return some kind of error
+			todo!();
 		}
-		println!("Connection!");
-		watcher.add(new_connection, Events::EPOLLIN)?;
-		todo!();
-		return Ok(());
+	
+		result.soc_addr.sin_addr.s_addr = INADDR_ANY.to_be();
+		result.soc_addr.sin_port = port.to_be();
+		result.soc_addr.sin_family = AF_INET as u16;
+	
+		if unsafe { fcntl(result.socket_fd, F_SETFL, O_NONBLOCK) }	< 0 {
+			unsafe { libc::close(result.socket_fd) };
+			// Return some kind of error;
+			todo!();
+		}
+	
+		if unsafe { bind(result.socket_fd,
+			&result.soc_addr as *const _ as *const _,
+			std::mem::size_of_val(&result.soc_addr) as u32) } != 0 {
+			unsafe { libc::close(result.socket_fd) };
+			// Return some kind of error
+			todo!();
+		}
+	
+		if unsafe { listen(result.socket_fd, 32) } != 0 {
+			unsafe { libc::close(result.socket_fd) };
+			// Return some kind of error
+			todo!();
+		}
+	
+		return result;
+	}
+	
+	pub fn get_new_connections(&self, watcher: &mut Watcher)
+		-> Result<(), Error> {
+		loop {
+			let new_connection = unsafe {
+				accept(self.socket_fd,
+					std::ptr::null_mut(),
+					std::ptr::null_mut())
+			};
+			if new_connection == -1 {
+				let last_error = Error::last_os_error();
+				if let Some(errno) = last_error.raw_os_error() {
+					if errno == EWOULDBLOCK || errno == EAGAIN {
+						return Ok(());
+					}
+				}
+				return Err(last_error);
+			}
+			println!("Connection!");
+			unsafe { fcntl(new_connection, F_SETFL, O_NONBLOCK) };
+			watcher.add(new_connection, Events::EPOLLIN)?;
+			return Ok(());
+		}
+	}
+
+	pub fn deconnection(&self, fd: i32, watcher: &mut Watcher)
+		-> Result<(), Error> {
+		watcher.delete(fd)?;
+		epoll::close(fd)?;
+		Ok(())
 	}
 }
 
-fn get_data_single_client() {
-	
+pub fn get_data(fd: i32) -> Result<Vec<String>, Error> {
+	let mut buf = [0 as u8; 1024];
+	let mut line = String::new();
+
+	loop {
+		match unsafe { libc::read(fd, buf.as_mut_ptr() as *mut c_void, 1024) } {
+			0 => {
+				break;
+			}
+			size => {
+				if size == -1 {
+					let last_error = Error::last_os_error();
+					if let Some(e) = last_error.raw_os_error() {
+						if e == EWOULDBLOCK {
+							break;
+						}
+						return Err(last_error);
+					}
+				}
+				line += String::from_utf8_lossy(&buf[0..size as usize]).to_string().as_str()
+			}
+		}
+	}
+	let lines: Vec<String> = line.split('\n').map(|l| l.to_string()).collect();
+	Ok(lines)
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -6,7 +6,7 @@
 /*   By: nguiard <nguiard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 09:08:14 by nguiard           #+#    #+#             */
-/*   Updated: 2024/03/05 13:09:09 by nguiard          ###   ########.fr       */
+/*   Updated: 2024/03/05 15:07:01 by nguiard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,52 +15,55 @@ mod watcher;
 
 use std::time::Duration;
 use epoll::Events;
-use libc::EPOLLIN;
+use libc::{EPOLLIN, EPOLLRDHUP};
 use structopt::StructOpt;
 
-use connections::{get_new_connections, init_socket};
+use connections::get_data;
 use watcher::Watcher;
+use crate::connections::ServerConnection;
 
 #[derive(StructOpt, Debug)]
 struct Args {
-    #[structopt(short, long, default_value = "4227")]
-    port: u16,
+	#[structopt(short, long, default_value = "4227")]
+	port: u16,
 
-    /// The map width
-    #[structopt(short, default_value = "180")]
-    x: u16,
+	/// The map width
+	#[structopt(short, default_value = "180")]
+	x: u16,
 
-    /// The map height
-    #[structopt(short, default_value = "100")]
-    y: u16,
+	/// The map height
+	#[structopt(short, default_value = "100")]
+	y: u16,
 
-    /// The team name(s)
-    #[structopt(short = "n", long)]
-    team_name: Vec<String>,
+	/// The team name(s)
+	#[structopt(short = "n", long)]
+	team_name: Vec<String>,
 
-    /// The number of clients authorized at the beginning of the game
-    #[structopt(short, long, default_value = "100")]
-    clients: u8,
+	/// The number of clients authorized at the beginning of the game
+	#[structopt(short, long, default_value = "100")]
+	clients: u8,
 
-    /// The time unit divider, every step of the server will go at 1/t second
-    #[structopt(short, long, default_value = "16")]
-    time: u8,
+	/// The time unit divider, every step of the server will go at 1/t second
+	#[structopt(short, long, default_value = "16")]
+	time: u8,
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let mut args = Args::from_args();
-    if args.team_name.is_empty() {
-        args.team_name.push("Blue team".into());
-    }
+	let mut args = Args::from_args();
+	if args.team_name.is_empty() {
+		args.team_name.push("Blue team".into());
+	}
+	dbg!(&args);
 	let tick_speed = Duration::from_secs_f64(1 as f64 / args.time as f64);
-
-	let con_data = init_socket(args.port);
+	dbg!(tick_speed);
+	let con_data = ServerConnection::init_socket(args.port);
 	let mut watcher = Watcher::new()?;
 
 	
 	watcher.add(con_data.socket_fd, Events::EPOLLIN)?;
 
 	loop {
+		println!("---");
 		let new_events = match watcher.update() {
 			Ok(events) => events,
 			Err(e) => {
@@ -70,15 +73,20 @@ fn main() -> Result<(), std::io::Error> {
 		};
 
 		if new_events.len() == 0 {
-			println!("No new events");
+			std::thread::sleep(tick_speed);
+			continue;
 		}
 
 		for event in new_events {
 			if event.data == con_data.socket_fd as u64 &&
 				event.events == EPOLLIN as u32 {
-				get_new_connections(&con_data, &mut watcher)?;
-			} else {
-				todo!();
+				con_data.get_new_connections(&mut watcher)?;
+			} else if event.events == EPOLLIN as u32 {
+				let lines = get_data(event.data as i32)?;
+				println!("{:?}", lines);
+			} else if event.events == EPOLLRDHUP as u32 {
+				println!("DECONNECTION!");
+				con_data.deconnection(event.data as i32, &mut watcher)?;
 			}
 		}
 
